eec57215cfa436eddb761c4f61a729143b52a089
Carles Pina i Estany <carles@pina.cat>
2022-08-02 20:21:41 +0100

    Add first view with a template - does nothing yet

--- kamus/core/urls.py
+++ kamus/core/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("translate/", views.Translate.as_view(), name="translate"),
     path("", views.Homepage.as_view(), name="homepage"),
 ]
--- kamus/core/views.py
+++ kamus/core/views.py
@@ -5,3 +5,18 @@ from django.views.generic import TemplateView
 
 class Homepage(TemplateView):
     template_name = "kamus/homepage.html"
+
+
+class Translate(TemplateView):
+    template_name = "kamus/translation.html"
+
+    def get_context_data(self, **kwargs):
+        word = self.request.GET.get("word", None)
+
+        if word is None:
+            word = "error"
+
+        context = {}
+        context["word"] = word
+
+        return context
