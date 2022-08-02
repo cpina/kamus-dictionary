93a21c74d6d29fff26a8cf157a7eb030a3cabfb6
Carles Pina i Estany <carles@pina.cat>
2022-08-02 23:20:06 +0100

    Add draftfor Docker

---
+++ kamus/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Used for the Docker deployments
+
+set -eu
+
+python3 manage.py check --deploy --fail-level WARNING
+python3 manage.py collectstatic --no-input --clear
+
+gunicorn kamus.wsgi:application \
+	--bind 0.0.0.0:8081 \
+	--workers=10 \
+	--timeout=600 \
+	--log-file=- \
+	--error-logfile=- \
+	--access-logfile=- \
+	--capture-output \
+	"$@"
