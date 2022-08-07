64af5356b5d1d6ec0bb990596539851d32c89ad7
Carles Pina i Estany <carles@pina.cat>
2022-08-07 19:28:36 +0100

    Open word selection on focus

--- kamus/core/static/js/enable-select2-bootstrap5-theme.js
+++ kamus/core/static/js/enable-select2-bootstrap5-theme.js
@@ -13,3 +13,16 @@ $(document).on('select2:open', (e) => {
 $('*').on('select2:select', function () {
     $('form')[0].submit();
 });
+
+// From: https://stackoverflow.com/a/49261426/9294284
+// on first focus (bubbles up to document), open the menu
+$(document).on('focus', '.select2-selection.select2-selection--single', function (e) {
+    $(this).closest(".select2-container").siblings('select:enabled').select2('open');
+});
+
+// steal focus during close - only capture once and stop propogation
+$('select.select2').on('select2:closing', function (e) {
+    $(e.target).data("select2").$selection.one('focus focusin', function (e) {
+        e.stopPropagation();
+    });
+});
