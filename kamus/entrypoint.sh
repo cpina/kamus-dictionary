ab90301611fa4bfb03fa112b4cc9fbd31354fac1
Carles Pina i Estany <carles@pina.cat>
2022-08-19 17:08:08 +0200

    Use pywikibot configuration
    Avoids warnings when using pywikibot

--- kamus/entrypoint.sh
+++ kamus/entrypoint.sh
@@ -4,14 +4,13 @@
 
 set -eu
 
-# TODO: not using yet the configuration but needs
-# to exist to make it use it for temporary files
-mkdir -p "$PYWIKIBOT_DIR"
-touch "$PYWIKIBOT_DIR/user-config.py"
-# since we deal with this we add user_agent_description
-# (TODO needs testing)
-# TODO: move to the config
-echo "user_agent_description = Kamus dictionary; https://kamus.pina.cat; carles@pina.cat"
+mkdir --parents "$PYWIKIBOT_DIR"
+
+cat <<EOT > "$PYWIKIBOT_DIR/user-config.py"
+family = 'wiktionary'
+mylang = 'en'
+user_agent_description = 'Kamus dictionary; https://kamus.pina.cat; carles@pina.cat'
+EOT
 
 # python3 manage.py check --deploy --fail-level WARNING
 python3 manage.py collectstatic --no-input --clear
