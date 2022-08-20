838767676eef2500471bb9c87a440dacd23a4dff
Carles Pina i Estany <carles@pina.cat>
2022-08-20 14:01:24 +0100

    Improve javascript and comments

--- kamus/core/static/js/setup-autocomplete.js
+++ kamus/core/static/js/setup-autocomplete.js
@@ -1,12 +1,15 @@
 $(document).ready(function () {
     $.fn.select2.defaults.set("theme", "bootstrap-5");
+
+    // If the user types something in the autocomplete and then click
+    // "Translate": it closes the autocomplete and submits the form
+    // (so the user does not need to close the autocomplete before
+    // pressing Translate)
     $.fn.select2.defaults.set("selectOnClose", true);
 });
 
 
-
-// Clicking the select2 opens the first option to start typing straight away
-// (also on Tab focus)
+// Clicking the select2 puts the cursor inside to start typing straight away
 $(document).on('select2:open', (e) => {
     const selectId = e.target.id;
     $(".select2-search__field[aria-controls='select2-" + selectId + "-results']").each(function (key, value,) {
@@ -14,9 +17,8 @@ $(document).on('select2:open', (e) => {
     });
 });
 
-// TODO: avoid using *
 // When click on an option: submit the form
-$('*').on('select2:select', function () {
+$(document).on('select2:select', function () {
     let form = $('form')[0];
 
     if (form.checkValidity()) {
@@ -27,11 +29,10 @@ $('*').on('select2:select', function () {
 });
 
 // From: https://stackoverflow.com/a/49261426/9294284
-// on first focus open the select2 (for example on TAB)
+// on focus open the select2 (for example on TAB)
 $(document).on('focus', '.select2-selection.select2-selection--single', function (e) {
     $(this).closest(".select2-container").siblings('select:enabled').select2('open');
 });
-
 // steal focus during close - only capture once and stop propagation
 $('select.select2').on('select2:closing', function (e) {
     $(e.target).data("select2").$selection.one('focus focusin', function (e) {
